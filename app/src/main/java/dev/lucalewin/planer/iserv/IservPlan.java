f11ef0ecc0826b12ad93809dc09f812758a2d9dc
Luca Lewin <luca.lewin23@gmail.com>
2022-03-02 20:35:21 +0100

    move table initialization

--- app/src/main/java/dev/lucalewin/planer/iserv/IservPlan.java
+++ app/src/main/java/dev/lucalewin/planer/iserv/IservPlan.java
@@ -1,10 +1,17 @@
 package dev.lucalewin.planer.iserv;
 
-import android.widget.TableRow;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Map;
+
+import dev.lucalewin.planer.R;
 
 public class IservPlan {
 
@@ -17,6 +24,31 @@ public class IservPlan {
     public IservPlan(DayOfWeek day, Map<String, List<IservPlanRow>> classes) {
         this.day = day;
         this.classes = classes;
+    }
+
+    /**
+     *
+     * @param context the context of the current activity
+     * @param clazz the class the user is in
+     * @return a list of table rows
+     */
+    public TableLayout initTable(Context context, String clazz) {
+        @SuppressLint("InflateParams")
+        TableLayout tableLayout = (TableLayout) LayoutInflater.from(context).inflate(R.layout.planer_table_layout, null, false);
+
+        final List<IservPlanRow> rows = classes.get(clazz);
+
+        if (rows != null) {
+            rows.forEach(row -> tableLayout.addView(row.toTableRow(context)));
+        } else {
+            tableLayout.findViewById(R.id.planer_table_header).setVisibility(View.GONE);
+
+            TextView notAffectedTextView = new TextView(context);
+            notAffectedTextView.setText(context.getString(R.string.not_affected));
+            tableLayout.addView(notAffectedTextView);
+        }
+
+        return tableLayout;
     }
 
     public DayOfWeek getDay() {
