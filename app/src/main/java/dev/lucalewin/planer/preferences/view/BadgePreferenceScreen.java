aa035b9b3e9928ffb07aa994c741b523af78b99a
Luca Lewin <luca.lewin23@gmail.com>
2022-03-02 20:37:56 +0100

    add Preferences with badges

---
+++ app/src/main/java/dev/lucalewin/planer/preferences/view/BadgePreferenceScreen.java
@@ -0,0 +1,64 @@
+package dev.lucalewin.planer.preferences.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import de.dlyt.yanndroid.oneui.preference.Preference;
+import de.dlyt.yanndroid.oneui.preference.PreferenceViewHolder;
+import dev.lucalewin.planer.R;
+
+public class BadgePreferenceScreen extends Preference {
+
+    public static final int N_BADGE = -1;
+
+    private TextView badgeNumberTextView;
+
+    private int badgeNumber = 0;
+
+    public BadgePreferenceScreen(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public BadgePreferenceScreen(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public BadgePreferenceScreen(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BadgePreferenceScreen(@NonNull Context context) {
+        super(context);
+    }
+
+    {
+        setWidgetLayoutResource(R.layout.badge_layout);
+    }
+
+    public void setBadge(int n) {
+        badgeNumber = n;
+        notifyChanged();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        badgeNumberTextView = (TextView) holder.findViewById(R.id.badge_number);
+
+        badgeNumberTextView.setVisibility(View.GONE);
+
+        if (badgeNumber == N_BADGE) {
+            badgeNumberTextView.setText("N");
+            badgeNumberTextView.setVisibility(View.VISIBLE);
+        } else if (badgeNumber > 0) {
+            badgeNumberTextView.setText(String.valueOf(badgeNumber));
+            badgeNumberTextView.setVisibility(View.VISIBLE);
+        }
+    }
+}
