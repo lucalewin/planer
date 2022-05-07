e2338c1bb7108d5e90d09a698a0566cc90c5177f
Luca Lewin <luca.lewin23@gmail.com>
2022-05-07 19:10:39 +0200

    add NonNull annotation to method `getSharedPreferences`

--- app/src/main/java/dev/lucalewin/planer/preferences/Preferences.java
+++ app/src/main/java/dev/lucalewin/planer/preferences/Preferences.java
@@ -3,13 +3,16 @@ package dev.lucalewin.planer.preferences;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import androidx.annotation.NonNull;
+
 import dev.lucalewin.planer.BuildConfig;
 
 public class Preferences {
 
     public static final String SHARED_PREFERENCES_NAME = BuildConfig.APPLICATION_ID + ".preferences";
 
-    public static SharedPreferences getSharedPreferences(Context context) {
+    @NonNull
+    public static SharedPreferences getSharedPreferences(@NonNull Context context) {
         return context.getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 
