e250b45479a095acc69f9354e9a11093f8725dfc
Luca Lewin <luca.lewin23@gmail.com>
2022-05-21 19:46:58 +0200

    remove commented out code

--- app/src/main/java/dev/lucalewin/planer/AboutActivity.java
+++ app/src/main/java/dev/lucalewin/planer/AboutActivity.java
@@ -14,11 +14,11 @@ public class AboutActivity extends BaseThemeActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        AboutPage about_page = findViewById(R.id.about_page);
+        final AboutPage about_page = findViewById(R.id.about_page);
 
         about_page.setUpdateState(AboutPage.LOADING);
 
-        UpdateManager updater = UpdateManager.getInstance(this);
+        final UpdateManager updater = UpdateManager.getInstance(this);
 
         new TaskRunner().executeAsync(updater::isLatestVersionInstalled, (result) -> {
             if (result) {
@@ -28,12 +28,5 @@ public class AboutActivity extends BaseThemeActivity {
                 about_page.setUpdateButtonOnClickListener(view -> updater.installLatestVersion());
             }
         });
-
-        // TODO
-//        ((MaterialButton) findViewById(R.id.about_btn1)).setOnClickListener(v -> about_page.setUpdateState(AboutPage.LOADING));
-//        ((MaterialButton) findViewById(R.id.about_btn2)).setOnClickListener(v -> about_page.setUpdateState(AboutPage.NO_UPDATE));
-//        ((MaterialButton) findViewById(R.id.about_btn3)).setOnClickListener(v -> about_page.setUpdateState(AboutPage.UPDATE_AVAILABLE));
-//        ((MaterialButton) findViewById(R.id.about_btn4)).setOnClickListener(v -> about_page.setUpdateState(AboutPage.NOT_UPDATEABLE));
-//        ((MaterialButton) findViewById(R.id.about_btn5)).setOnClickListener(v -> about_page.setUpdateState(AboutPage.NO_CONNECTION));
     }
 }
