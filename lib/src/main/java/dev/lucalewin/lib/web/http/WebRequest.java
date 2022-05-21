75b84ac64ff227ecd44158a6934db944b10ed09a
Luca Lewin <luca.lewin23@gmail.com>
2022-05-21 19:42:16 +0200

    add builder for `WebRequest`

--- lib/src/main/java/dev/lucalewin/lib/web/http/WebRequest.java
+++ lib/src/main/java/dev/lucalewin/lib/web/http/WebRequest.java
@@ -1,5 +1,6 @@
 package dev.lucalewin.lib.web.http;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -81,5 +82,53 @@ public class WebRequest {
 
     public void setResponseCharset(Charset responseCharset) {
         this.responseCharset = responseCharset;
+    }
+
+    public static WebRequestBuilder builder(String url) throws MalformedURLException {
+        return new WebRequestBuilder(url);
+    }
+
+    public static class WebRequestBuilder {
+
+        private final WebRequest req;
+
+        public WebRequestBuilder(String url) throws MalformedURLException {
+            req = new WebRequest(new URL(url));
+        }
+
+        public WebRequestBuilder setMethod(HttpMethod method) {
+            req.setMethod(method);
+            return this;
+        }
+
+        public WebRequestBuilder addHeaderField(String key, String value) {
+            req.addHeaderField(key, value);
+            return this;
+        }
+
+        public WebRequestBuilder setBody(String body) {
+            req.setBody(body);
+            return this;
+        }
+
+        public WebRequestBuilder setConnectionTimeout(int connectionTimeout) {
+            req.setConnectionTimeout(connectionTimeout);
+            return this;
+        }
+
+        public WebRequestBuilder setRequestCharset(Charset charset) {
+            req.setRequestCharset(charset);
+            return this;
+        }
+
+        public WebRequestBuilder setResponseCharset(Charset charset) {
+            req.setResponseCharset(charset);
+            return this;
+        }
+
+        public WebRequest build() {
+            return req;
+        }
+
     }
 }
